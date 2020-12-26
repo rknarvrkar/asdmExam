b39f75ae5776e4963a72293608abd9a3491516e7
rknarvrkar <71268183+rknarvrkar@users.noreply.github.com>
2020-12-26 11:18:24 +0530

    First Commit

---
+++ validation.js
@@ -0,0 +1,35 @@
+
+$(function() {
+    
+    $("form[name='registration']").validate({
+      // Specify validation rules
+      rules: {
+        
+        firstname: "required",
+        lastname: "required",
+        email: {
+          required: true,
+          
+          email: true
+        },
+        password: {
+          required: true,
+          minlength: 5
+        }
+      },
+      // Specify validation error messages
+      messages: {
+        firstname: "Please enter your firstname",
+        lastname: "Please enter your lastname",
+        password: {
+          required: "Please provide a password",
+          minlength: "Your password must be at least 5 characters long"
+        },
+        email: "Please enter a valid email address"
+      },
+      
+      submitHandler: function(form) {
+        form.submit();
+      }
+    });
+  });
